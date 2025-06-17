ea93d925309c931acc8e2929a3229f5d136301c4
Hummner <bence.cservenyak@gmail.com>
2025-06-17 07:56:20 +0200

    Filter and sort added to User Page

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -18,13 +18,20 @@ import { Firestore } from '@angular/fire/firestore';
 import { collection, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { DocumentData } from "firebase/firestore";
 import { RouterLink } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule, RouterLink],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule, RouterLink, MatInputModule, FormsModule, MatIconModule, MatButtonModule, MatSlideToggleModule, MatButtonToggleModule, MatSelectModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
@@ -34,11 +41,17 @@ export class UserComponent implements OnInit, OnDestroy {
   firestore: Firestore;
   unsub: any;
   allUsers: DocumentData[] = [];
+  filterValue: string = '';
+
+  sortField: string = 'firstname';
+  sortDirection: 'asc' | 'desc' = 'asc';
+  hideSingleSelectionIndicator = signal(false);
 
 
 
   constructor(public dialog: MatDialog) {
     this.firestore = inject(Firestore);
+
 
 
     this.unsub = onSnapshot(this.usersCollectionRef(), (users) => {
@@ -103,4 +116,35 @@ export class UserComponent implements OnInit, OnDestroy {
     });
   }
 
+  toggleSingleSelectionIndicator() {
+    this.hideSingleSelectionIndicator.update(value => !value);
+  }
+
+
+
+  toggleSortDirection() {
+    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+  }
+
+
+  get setFilter() {
+
+
+    let filter = this.filterValue.toLowerCase();
+    let filtered = this.allUsers.filter(user =>
+      user['firstname']?.toLowerCase().includes(filter) ||
+      user['lastname']?.toLowerCase().includes(filter) ||
+      user['email']?.toLowerCase().includes(filter)
+    );
+
+    return filtered.sort((a, b) => {
+      const aVal = a[this.sortField]?.toLowerCase?.() || '';
+      const bVal = b[this.sortField]?.toLowerCase?.() || '';
+
+      if (aVal < bVal) return this.sortDirection === 'asc' ? -1 : 1;
+      if (aVal > bVal) return this.sortDirection === 'asc' ? 1 : -1;
+      return 0;
+    })
+
+  }
 }
